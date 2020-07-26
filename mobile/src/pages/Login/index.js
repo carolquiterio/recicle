3a98f1c691b0476b17db86471d864d1bda4b807d
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-07-26 00:55:18 -0300

    Login page

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, KeyboardAvoidingView, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, KeyboardAvoidingView, Image, TouchableOpacity, StyleSheet, Button } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 import logoImage from '../../assets/logo.png';
@@ -7,10 +7,11 @@ import logoImage from '../../assets/logo.png';
 
 export default function Login() {
     return(
+        
         <KeyboardAvoidingView style={styles.background}>
-            <View>
-                
-            </View>  
+        <View>
+            <Image style={styles.image} source={logoImage}/>       
+        </View>  
 
             <View>
                 <TextInput
@@ -33,33 +34,47 @@ export default function Login() {
                     //returnKeyType = { "next" }
                 />
 
-                <TouchableOpacity
-                style={styles.button}>
+                <TouchableOpacity style={styles.button}>
                     <Text style={styles.buttonText}>Pronto!</Text>
                 </TouchableOpacity>
-
+                <Text style={styles.text}>Esqueci minha senha!</Text>
+                
+                <Text style={styles.text}>Ainda não tem conta? <Text style={styles.greenText}>Criar</Text></Text>
             </View>
         </KeyboardAvoidingView>
+        
     );
 }
 
 const styles = StyleSheet.create({  
     
     image: {
-        maxHeight: 230,
-        maxWidth: 450
+        maxHeight: 200,
+        maxWidth: 450,
+        marginBottom: 50, 
+        justifyContent: 'flex-start'
     },
 
-    input: {
+    input: {  
         backgroundColor: '#F5EEF5',
         borderRadius: 30,
         paddingLeft: 20,
         minWidth:350,
-        marginBottom: 20
+        marginBottom: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+        fontFamily: 'Roboto',
+        fontSize: 16
     },
 
     background: {
-        flex: 1,
+        flex: 0,
         alignItems:'center',
         justifyContent:'center',
     },
@@ -71,7 +86,16 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         minHeight: 50,
         alignItems: 'center',
-        fontWeight: '500'
+        fontWeight: '500',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+        fontFamily: 'Roboto'
     },
 
     buttonText: {
@@ -79,7 +103,31 @@ const styles = StyleSheet.create({
         paddingTop: 13,
         textAlign: 'center',
         fontSize: 16,
-        fontWeight: '500'
+        fontWeight: '500',
+        fontFamily: 'Roboto',
+    },
+
+    text: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 18,
+        lineHeight: 21,
+        color: '#757575',
+        alignSelf: 'center',
+        marginTop: 10,
+        marginBottom: 140
+    },
+
+    greenText: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 18,
+        lineHeight: 21,
+        color: '#0EB161',
+        alignSelf: 'center',
+        marginTop: 50,
     }
   });
   
