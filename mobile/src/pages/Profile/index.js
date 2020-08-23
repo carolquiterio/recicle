dbd903c98f91eac25261efe6e9cc2223e1d41e93
Aluno41 <aluno@dinalva41.academico.cotuca.unicamp.br>
2020-08-22 22:06:33 -0300

    profile

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import ImageCarol from '../../assets/carol.jpg';
 
-import {StyledContainer, StyledText} from './styles';
+import {StyledContainer, StyledText, StyledText1, ArvoreContainer, ContainerCarol} from './styles';
 
 export default function Profile() {
   return (
     <StyledContainer>
-      <StyledText>Profile</StyledText>
+    <View> 
+
+      <ContainerCarol>
+        <Image source={ImageCarol} />
+      </ContainerCarol>
+
+      <StyledText1>
+        <Text>@Carol_quiterio</Text>
+      </StyledText1>
+
+      <ArvoreContainer>
+        <StyledText>
+          <Text>Você salvou 99 arvores...</Text>
+        </StyledText>  
+      </ArvoreContainer>
+
+      </View>
     </StyledContainer>
   );
 }
--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -2,11 +2,53 @@ import styled from 'styled-components/native';
 
 export const StyledContainer = styled.View`
   flex: 1;
-  padding: 20px;
-  justify-content: center;
-  align-items: center;
+  color: #EFEFEF;
 `;
 
 export const StyledText = styled.Text`
   font-size: 20px;
+  color: #ffffff;
 `;
+
+export const StyledText1 = styled.Text`
+  font-size: 17px;
+  color: #000000;
+  left: 125px;
+  top: -70px;
+`;
+
+export const LogoContainerMenu = styled.View`
+  height: 100%;
+  width: 60%;
+  align-items:center;
+  transform: scale(0.9);
+`;
+
+export const ImageLogo = styled.Image`
+  width: 10%;
+`;
+export const ContainerCarol = styled.View`
+  height: 50%;
+  width: 100%;
+  padding: -130px;
+  justify-content: center;
+  align-items: center;
+  transform: scale(0.2);
+  border-radius: 700px;
+`;
+
+export const ImageCarol = styled.Image`
+  width: 100%; 
+`;
+export const  ArvoreContainer = styled.View`
+  background-color: #34CB79;
+  height:30%;
+  width: 80%;
+  padding-right: 40px;
+  top: -50px;
+  left: 40px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+`;
+
