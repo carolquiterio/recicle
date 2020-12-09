d2ddbb20a4dc3203daaf1b981b1296ff647112ca
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-12-08 21:42:01 -0300

    collect changes

--- mobile/src/components/CollectItem/index.js
+++ mobile/src/components/CollectItem/index.js
@@ -1,45 +1,55 @@
-import React from  'react';
-import { View, Image, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import React from 'react';
+import {View, Image, Text, Linking} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
 
 import styles from './styles';
 import whatsappIcon from '../../assets/icons/whatsapp.png';
 import emailIcon from '../../assets/icons/mail-white.png';
 
-function CollectItem() {
-    return (
-      <View style={styles.container}>
-          <View style={styles.profile}>
-            <Image
-             style={styles.img}
-             source={{ uri: 'https://i0.wp.com/www.larplasticos.com.br/wp-content/uploads/2018/07/lixeiras-de-coleta-seletiva-larplasticos.jpg?fit=700%2C420&ssl=1'}}
-            />
-          </View>
+function CollectItem(props) {
+  function handleWhatsPress() {
+    Linking.openURL('sms:188');
+  }
 
-            <View style={styles.infoColeta}>
-              <Text style={styles.title}>Colectoria</Text>
-              <Text style={styles.subTitle}>Resíduos Eletrônicos, Lâmpadas, Pilhas e Baterias</Text>
-              <Text style={styles.description}>Rio do Sul, santa catarina </Text>
-              <Text style={styles.endereco}>
-                  Endereço {'\n'}
-                 
-                  <Text style={styles.descriptionEndereco}>Rua fulano de tal</Text>
-              </Text>
-            </View>
-              
-            <View style={styles.buttonsContainer}>
-              <RectButton style={styles.whatsButton}>
-                <Image source={whatsappIcon}/>
-                <Text style={styles.whatsButtonText}>Whatsapp</Text>
-              </RectButton>
+  function handleEmailPress() {
+    Linking.openURL('mailto:ong@gmail.org');
+  }
 
-              <RectButton style={styles.emailButton}>
-                <Image source={emailIcon}/>
-                <Text style={styles.emailButtonText}>E-mail</Text>
-              </RectButton>
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.img}
+          source={{
+            uri:
+              'https://i0.wp.com/www.larplasticos.com.br/wp-content/uploads/2018/07/lixeiras-de-coleta-seletiva-larplasticos.jpg?fit=700%2C420&ssl=1',
+          }}
+        />
       </View>
-    )
+
+      <View style={styles.infoColeta}>
+        <Text style={styles.title}>{props.collectName}</Text>
+        <Text style={styles.subTitle}>{props.items}</Text>
+        <Text style={styles.description}>{props.description}</Text>
+        <Text style={styles.endereco}>
+          Endereço {'\n'}
+          <Text style={styles.descriptionEndereco}>{props.place}</Text>
+        </Text>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton style={styles.whatsButton} onPress={handleWhatsPress}>
+          <Image source={whatsappIcon} />
+          <Text style={styles.whatsButtonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.emailButton} onPress={handleEmailPress}>
+          <Image source={emailIcon} />
+          <Text style={styles.emailButtonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </View>
+  );
 }
 
 export default CollectItem;
--- mobile/src/pages/Collect/index.js
+++ mobile/src/pages/Collect/index.js
@@ -7,7 +7,13 @@ export default function Collect() {
   return (
     <StyledContainer>
       <StyledText>Collect</StyledText>
-      <CollectItem />
+
+      <CollectItem
+        collectName="ONG Coleta"
+        description="Somos uma ONG que recebe materiais e faz sua redistribuição e venda para outros lugares, usando todo o lucro para ajudar animais de rua"
+        items="Baterias, pilhas, metais etc"
+        place="Rua Paula Bueno 877 - Taquaral - Campinas"
+      />
     </StyledContainer>
   );
 }
