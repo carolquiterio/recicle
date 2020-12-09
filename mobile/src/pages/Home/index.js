2a22805203138ae28ab2a6605bbcc8ea6bb11404
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-12-09 12:13:46 -0300

    progress

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -3,17 +3,37 @@ import {ScrollView, View} from 'react-native';
 
 import Grafic from '../../components/Grafic';
 
-import {StyledContainer, Title, GraficContainer} from './styles';
+import {
+  StyledContainer,
+  Title,
+  GraficContainer,
+  StyledButton,
+  ButtonContainer,
+  StyledText,
+  StyledWhite,
+  StyledView,
+  ButtonCategorie,
+} from './styles';
 
 export default function Home(props) {
   return (
-    <View>
+    <StyledView>
       <StyledContainer>
+        <ButtonContainer>
+          <StyledButton>
+            <StyledText>Dia</StyledText>
+          </StyledButton>
+
+          <StyledWhite>
+            <StyledText>Mes</StyledText>
+          </StyledWhite>
+        </ButtonContainer>
         <Title>Seu progresso</Title>
         <GraficContainer>
           <Grafic />
         </GraficContainer>
+        <ButtonCategorie></ButtonCategorie>
       </StyledContainer>
-    </View>
+    </StyledView>
   );
 }
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -2,6 +2,12 @@ import styled from 'styled-components/native';
 
 export const StyledContainer = styled.View`
   flex: 1;
+
+  justify-content: center;
+`;
+
+export const StyledView = styled.View`
+  flex: 1;
   padding: 20px;
   justify-content: center;
 `;
@@ -19,13 +25,57 @@ export const Title = styled.Text`
   font-size: 20px;
   line-height: 23px;
   /* identical to box height */
-
   color: #363636;
+  margin-bottom: 5px;
 `;
 
 export const GraficContainer = styled.View`
-  padding-bottom: 10px;
+  padding: 10px;
   background-color: #fff;
+  border-radius: 10px;
 `;
 
-export const
+export const ButtonContainer = styled.View`
+  background: #fff;
+  border-radius: 30px;
+  justify-content: space-around;
+  flex-direction: row;
+  margin-bottom: 18px;
+  height: 40px;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  background: #34cb79;
+  border-radius: 30px;
+  width: 50%;
+  align-items: center;
+  height: 100%;
+  justify-content: center;
+`;
+
+export const StyledWhite = styled.TouchableOpacity`
+  background: #fff;
+  border-radius: 30px;
+  width: 50%;
+  align-items: center;
+  height: 100%;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: Ubuntu;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+export const ButtonCategorie = styled.TouchableOpacity`
+  background: #fff;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+`;
